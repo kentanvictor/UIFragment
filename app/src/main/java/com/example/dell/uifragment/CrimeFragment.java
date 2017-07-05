b2e0bff0ce4c0b44d4e9a486e228cd0e936260bd
kentanvictor <2030457926@qq.com>
2017-07-05 20:11:42 +0800

    second commit

--- app/src/main/java/com/example/dell/uifragment/CrimeFragment.java
+++ app/src/main/java/com/example/dell/uifragment/CrimeFragment.java
@@ -7,7 +7,9 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by DELL on 2017/7/5.
@@ -16,6 +18,7 @@ import android.widget.EditText;
 public class CrimeFragment extends Fragment {
     Crime mCrime;
     EditText edit1;
+    Button addUp;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,8 +29,14 @@ public class CrimeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, parent, false);
-
+        addUp = (Button) v.findViewById(R.id.but1);
         edit1 = (EditText) v.findViewById(R.id.crime_title);
+        addUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "be clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
         edit1.addTextChangedListener(new TextWatcher() {
             public void onTextChanged(CharSequence c, int start, int before, int count) {
                 mCrime.setTitle(c.toString());
