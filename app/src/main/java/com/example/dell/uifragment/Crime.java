a28d96f3c90ff30768d317bdde7e38b1c4d0fe0b
kentanvictor <2030457926@qq.com>
2017-07-05 21:29:42 +0800

    second commit

--- app/src/main/java/com/example/dell/uifragment/Crime.java
+++ app/src/main/java/com/example/dell/uifragment/Crime.java
@@ -1,5 +1,6 @@
 package com.example.dell.uifragment;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -11,8 +12,14 @@ public class Crime {
 
     private String mTitle;
 
+    private Date mDate;
+
+
+    private boolean mSolved;
+
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public String getTitle() {
@@ -26,4 +33,21 @@ public class Crime {
     public UUID getId() {
         return mId;
     }
+
+    public Date getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean ismSolved() {
+        return mSolved;
+    }
+
+    public void setmSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
+
 }
